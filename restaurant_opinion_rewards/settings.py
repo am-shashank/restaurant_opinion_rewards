ce7e291cbe4d90c8fa91391b43c92579738b66d9
shashank <am.seshank@gmail.com>
2015-04-11 23:05:18 -0400

    database models created

--- restaurant_opinion_rewards/settings.py
+++ restaurant_opinion_rewards/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'restaurant_opinion_rewards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ResOpRew',
+        'USER': 'awsmasteruser',
+        'PASSWORD': 'brogrammer',
+        'HOST': 'bitsplease.cdudfd8rdw9w.us-east-1.rds.amazonaws.com',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
